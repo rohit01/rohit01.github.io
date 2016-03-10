2a470ca5537d5a60528a0567ae0d8680b6c37ffa
rohit01 <hello@rohit.io>
2016-03-11 04:00:30 +0530

    Adding deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Build and deploy jeky site..
+#
+
+echo "git checkout dev"
+git checkout dev
+
+echo "Building site..."
+jekyll build
+
+echo "git checkout master"
+git checkout master
+
+echo "Committing files..."
+cp -r _site/* ./
+git rm --ignore-unmatch $(git ls-files --deleted) ./._.inva.lid_78.${RANDOM}.99
+git add ./
+git commit -m "Release $(date +%b_%d_%G_%Hh%Mm%Ss)"
+git push
+
+echo "git checkout dev"
+git checkout dev
