2645bd011d229998c37bfaa83c2e9c334b8e956c
rohit.gupta2 <rohit.gupta2@olacabs.com>
2016-04-01 04:16:16 +0530

    tweaks

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ git checkout dev
 echo "Building site..."
 jekyll build
 
+# Backup .gitmodules
+cp .gitmodules .gitmodules.backup
+
 echo "git checkout master"
 git checkout master
 
@@ -17,22 +20,28 @@ echo "Committing files..."
 # Save site and submodules
 mv ./_site ./._site
 mv ./photos ./.photos
+
 # Delete existing files
 git rm -r *
 rm -rf *
+
 # Add files
 mv ./._site ./_site
 mv ./.photos ./photos
 cp -r _site/* ./
-# Add submodules
-git add ./photos
-# Add site
-git add ./
+cp .gitmodules.backup .gitmodules
+rm .gitmodules.backup
 
-git rm --ignore-unmatch $(git ls-files --deleted) ./._.inva.lid_78.${RANDOM}.99
+# Add website
+git add ./ ./photos ./.gitmodules
 
+# Commit and push
 git commit -m "Release $(date +%b_%d_%G_%Hh%Mm%Ss)"
 git push
 
+# back to dev branch
 echo "git checkout dev"
 git checkout dev
+
+# Not required anymore..
+# git rm --ignore-unmatch $(git ls-files --deleted) ./._.inva.lid_78.${RANDOM}.99
