ed265f77709f9297b73e8003c922d1ee8033270e
rohit.gupta2 <rohit.gupta2@olacabs.com>
2016-04-01 04:13:00 +0530

    Tweaks

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,24 @@ echo "git checkout master"
 git checkout master
 
 echo "Committing files..."
+
+# Save site and submodules
+mv ./_site ./._site
+mv ./photos ./.photos
+# Delete existing files
+git rm -r *
+rm -rf *
+# Add files
+mv ./._site ./_site
+mv ./.photos ./photos
 cp -r _site/* ./
-git add ./photos/album/*
+# Add submodules
+git add ./photos
+# Add site
 git add ./
+
 git rm --ignore-unmatch $(git ls-files --deleted) ./._.inva.lid_78.${RANDOM}.99
+
 git commit -m "Release $(date +%b_%d_%G_%Hh%Mm%Ss)"
 git push
 
