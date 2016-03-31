faf7df2e53334169e67c90044b179677c5550dc6
rohit01 <hello@rohit.io>
2016-03-31 23:07:50 +0000

    Release Mar_31_2016_23h07m50s

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,47 @@ echo "git checkout dev"
 git checkout dev
 
 echo "Building site..."
+rm -rf _site/
 jekyll build
+
+echo "Backup .gitmodules & .gitignore"
+cp .gitmodules .gitmodules.backup
+cp .gitignore .gitignore.backup
 
 echo "git checkout master"
 git checkout master
 
 echo "Committing files..."
-cp -r _site/* ./
-git rm --ignore-unmatch $(git ls-files --deleted) ./._.inva.lid_78.${RANDOM}.99
-git add ./
+
+echo "Save site and submodules"
+mv ./_site ./._site
+mv ./photos ./.photos
+
+echo "Delete existing files"
+rm -rf *
+git rm -r *
+
+echo "Add files"
+mv ./._site ./_site
+mv _site/* ./
+rm -rf _site/
+echo "Add photos"
+rm -rf ./photos
+mv ./.photos ./photos
+echo "Add .gitmodules & .gitignore"
+cp .gitmodules.backup .gitmodules
+cp .gitignore.backup .gitignore
+rm .gitmodules.backup .gitignore.backup
+
+echo "git add website"
+git add ./ ./photos ./.gitmodules
+
+echo "Commit and push"
 git commit -m "Release $(date +%b_%d_%G_%Hh%Mm%Ss)"
 git push
 
 echo "git checkout dev"
 git checkout dev
+
+# Not required anymore..
+# git rm --ignore-unmatch $(git ls-files --deleted) ./._.inva.lid_78.${RANDOM}.99
